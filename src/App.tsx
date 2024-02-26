42be589cf9842579aa7cda111d9bdcf0cd421b25
Sonya <sonya.castro.gomez@gmail.com>
2024-02-26 10:11:17 -0500

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
-import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import papers from "./data/papers.json";
-import Table from './components/table/Table';
-import { useState } from 'react';
+import './App.css'
+import PapersPage from './ui/pages/PapersPage'
+import NewsPage from './ui/pages/NewsPage'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PapersPage />
+  },
+  {
+    path: "/news",
+    element: <NewsPage />
+  }
+]);
+
 
 function App() {
-
-  // const journals = [...new Set(papers.map(papers => papers.journal))]
-
-  const [filteredPapers, setfilteredPapers] = useState(papers);
-
-  const handleSearch = (event: React.SyntheticEvent): void => {
-    const target = event.target as typeof event.target & {
-      value: string
-    }
-
-    if (target.value.length < 0) {
-      setfilteredPapers(papers);
-      return;
-    }
-
-    const regExp = new RegExp(target.value + "+", "i");
-    setfilteredPapers(papers.filter(paper => regExp.test(paper.title + " " + paper.authors.join(" "))))
-  }
-
-  return (
-    <div className='w-full p-5'>
-      <div className='w-full'>
-        <label htmlFor="search-box">Search</label>
-        <input placeholder='Type to Search...' type="text" name="search-box" id="search-box" className='block w-full p-3 border border-cyan-900 focus:border-cyan-600 focus:border-2 outline-none rounded drop-shadow-md' onChange={handleSearch} />
-      </div>
-
-      <Table data={filteredPapers} headers={["authors", "year", "title", "journal"]} className='mt-5 ' />
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/ui/pages/PapersPage.tsx
@@ -0,0 +1,37 @@
+
+import papers from "../../data/papers.json";
+import Table from '../components/Table';
+import { useState } from 'react';
+
+export default function PapersPage() {
+
+    // const journals = [...new Set(papers.map(papers => papers.journal))]
+
+    const [filteredPapers, setfilteredPapers] = useState(papers);
+
+    const handleSearch = (event: React.SyntheticEvent): void => {
+        const target = event.target as typeof event.target & {
+            value: string
+        }
+
+        if (target.value.length < 0) {
+            setfilteredPapers(papers);
+            return;
+        }
+
+        const regExp = new RegExp(target.value + "+", "i");
+        setfilteredPapers(papers.filter(paper => regExp.test(paper.title + " " + paper.authors.join(" "))))
+    }
+
+    return (
+        <div className='w-full p-5'>
+            <div className='w-full'>
+                <label htmlFor="search-box">Search</label>
+                <input placeholder='Type to Search...' type="text" name="search-box" id="search-box" className='block w-full p-3 border border-cyan-900 focus:border-cyan-600 focus:border-2 outline-none rounded drop-shadow-md' onChange={handleSearch} />
+            </div>
+
+            <Table data={filteredPapers} headers={["authors", "year", "title", "journal"]} className='mt-5 ' />
+        </div>
+    )
+}
+
